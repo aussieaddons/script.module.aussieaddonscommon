3b5ed0ab435e59fc79f0754f4f4d84578cedbbe2
Andy Botting <andy@andybotting.com>
2017-08-10 12:57:31 +1000

    Update reporting token again after it went missing

--- lib/aussieaddonscommon/issue_reporter.py
+++ lib/aussieaddonscommon/issue_reporter.py
@@ -12,8 +12,8 @@ from distutils.version import LooseVersion
 
 
 GITHUB_API_URL = 'https://api.github.com/repos/xbmc-catchuptv-au/issue-reports'
-# aussieaddonsbot token
-GITHUB_API_TOKEN = '942825814667e2d030a489eb2de9893d6d9be2d9'
+# aussieaddonsbot reporting token
+GITHUB_API_TOKEN = '56530f7a910d58ea19af1e15ed684a418084852c'
 ISSUE_API_URL = GITHUB_API_URL + '/issues'
 GIST_API_URL = 'https://api.github.com/gists'
 
