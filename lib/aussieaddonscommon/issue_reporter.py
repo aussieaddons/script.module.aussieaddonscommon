7246d74f13dbb0950e879c9654e9ba2dae1a1bf6
Glenn Guy <glennguy83@gmail.com>
2018-11-13 23:37:57 +1100

    Add additional orgs to blacklist

--- lib/aussieaddonscommon/issue_reporter.py
+++ lib/aussieaddonscommon/issue_reporter.py
@@ -208,8 +208,15 @@ def blacklisted_hostname(connection_info):
     Some VPNs/proxys are known to content providers and will return 403
     responses. Blacklisting these to avoid issues reports caused by this.
     """
-    org_blacklist = ['highwinds', 'softlayer', 'micfo']
-    hostname_blacklist = ['ipvanish', 'zoogvpn', 'sl-reverse']
+    org_blacklist = ['highwinds',
+                     'softlayer',
+                     'micfo',
+                     'total server solutions',  # PIA
+                     'host universal pty ltd']  # NordVPN
+
+    hostname_blacklist = ['ipvanish',
+                          'zoogvpn',
+                          'sl-reverse']
 
     if not connection_info:
         return False
